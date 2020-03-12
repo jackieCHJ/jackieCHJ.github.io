3138d44eb3050193b60f50b7c6fa3c5524a6e405
jackieCHJ <61968262+jackieCHJ@users.noreply.github.com>
2020-03-13 04:06:44 +0800

    Add files via upload

---
+++ js/readjson.js
@@ -0,0 +1,213 @@
+			var handata=new Array();
+			var rdata=new Array();
+			var jdata=new Array();
+			var ydata=new Array();
+			var mdata=new Array();
+			var edata=new Array();
+			var idata=new Array();
+			var bdata=new Array();
+			var worlddata=new Array();
+			var worldsumdata=new Array();
+			var dataobject;
+			var citydata=new Array();
+	$(function(){
+
+			$.get("https://jackiechj.github.io/first/data/hg.json",function(msg){       //请求返回参数
+		handata=msg;
+			});
+			$.get("https://jackiechj.github.io/first/data/rb.json",function(msg){       //请求返回参数
+		rdata=msg;
+			});
+			$.get("https://jackiechj.github.io/first/data/jnd.json",function(msg){       //请求返回参数
+		jdata=msg;
+			});
+			$.get("https://jackiechj.github.io/first/data/yd.json",function(msg){       //请求返回参数
+		ydata=msg;
+			});
+			$.get("https://jackiechj.github.io/first/data/mg.json",function(msg){       //请求返回参数
+		mdata=msg;
+			});
+			$.get("https://jackiechj.github.io/first/data/eg.json",function(msg){       //请求返回参数
+		edata=msg;
+			});
+			$.get("https://jackiechj.github.io/first/data/idl.json",function(msg){       //请求返回参数
+		idata=msg;
+			});
+			$.get("https://jackiechj.github.io/first/data/bx.json",function(msg){       //请求返回参数
+		bdata=msg;
+			});
+			$.get("https://jackiechj.github.io/first/data/world.json",function(msg){       //请求返回参数
+		worlddata=msg;
+			});
+			$.get("https://jackiechj.github.io/first/data/worldsum.json",function(msg){       //请求返回参数
+		worldsumdata=msg;
+			});
+			$.get("https://jackiechj.github.io/first/data/data.json",function(msg){       //请求返回参数
+		dataobject=msg;
+			});
+			$.get("https://jackiechj.github.io/first/json/city1.json",function(msg){       //请求返回参数
+		citydata=msg;
+			});
+		
+		
+var dom = document.getElementById("container");
+var myChart = echarts.init(dom);
+var app = {};
+option = null;
+myChart.showLoading();
+var convertData = function (dataworld,datacity) {
+    var res = [];
+    for (var i = 0; i < datacity.length; i++) {
+		var pro=datacity[i].properties;
+		var adoname=pro.name;
+		var newArr = dataworld.filter(function (obj) {
+		return obj.name== adoname;
+});
+        
+        if (newArr.length!=0) {
+			var vvalue=newArr[0].value;
+			var geome=datacity[i].geometry;
+			var coor=geome.coordinates;
+			var arr=[];arr.push(coor);
+			coor.push(vvalue);
+            res.push({
+                name: adoname,
+                value: coor
+				//geoCoord.concat(data[i].value)
+            });
+        }
+    }
+    return res;
+};
+
+$.get('https://jackiechj.github.io/first/json/world1.json', function (geoJson) {
+
+    myChart.hideLoading();
+
+    echarts.registerMap('world', geoJson);
+	var val=convertData(worlddata,citydata.features);
+
+    myChart.setOption(option = {
+        title: {
+            text: '世界疫情分布',
+            //subtext: '人口密度数据来自Wikipedia',
+            //sublink: 'http://zh.wikipedia.org/wiki/%E9%A6%99%E6%B8%AF%E8%A1%8C%E6%94%BF%E5%8D%80%E5%8A%83#cite_note-12'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: function (params, ticket, callback) {
+        console.log(params);
+        var showHtm="";
+        
+        showHtm+="国家："+params.name+ "确诊人数："+ params.value[2]+" 人"+'<br>'
+  
+        return showHtm;
+        }},
+        toolbox: {
+            show: true,
+            orient: 'vertical',
+            left: 'right',
+            top: 'center',
+            feature: {
+                dataView: {readOnly: false},
+                restore: {},
+                saveAsImage: {}
+            }
+        },
+		geo: {
+        map: 'world',
+		zoom: 1.2,
+		aspectScale:1.2,
+        itemStyle: {					// 定义样式
+            normal: {					// 普通状态下的样式
+                areaColor: '#323c48',
+                borderColor: '#111'
+            },
+            emphasis: {					// 高亮状态下的样式
+                areaColor: '#2a333d'
+            }
+        }
+    },
+        /* visualMap: {
+            min: 0,
+            max: 1000,
+            text: ['High', 'Low'],
+            realtime: false,
+            calculable: true,
+            inRange: {
+                color: ['lightskyblue', 'yellow', 'orangered']
+            }
+        }, */
+        series: [{
+            name: 'pm2.5',
+            type: 'scatter',
+            coordinateSystem: 'geo',
+            data: val,
+            /* symbolSize: function (val) {
+                return val[2] / 1000;
+            }, */
+            label: {
+                formatter: '{b}',
+                position: 'right',
+                show: true
+            },
+            itemStyle: {
+                color: 'red'
+            },
+            emphasis: {
+                label: {
+                    show: false
+                }
+            }
+        }/* ,
+            {
+                name: '香港18区人口密度',
+                type: 'map',
+                mapType: 'world', // 自定义扩展图表类型
+				zoom: 1.2,
+				aspectScale:1.2,
+				//width:120%,
+                label: {
+                    show: false
+                },
+                data: worlddata 
+                 //自定义名称映射
+                 
+            } */
+			
+        
+			/* ,
+			
+			{
+            name: 'pm2.5',
+            type: 'scatter',
+            coordinateSystem: 'bmap',
+            data: convertData(data1),
+            symbolSize: function (val) {
+                return val[2] / 10;
+            },
+            label: {
+                formatter: '{b}',
+                position: 'right',
+                show: false
+            },
+            itemStyle: {
+                color: 'purple'
+            },
+            emphasis: {
+                label: {
+                    show: true
+                }
+            }
+        } */
+        ]
+    });
+});;
+if (option && typeof option === "object") {
+    myChart.setOption(option, true);
+}
+  });
+
+  console.log(worlddata);
+  
+	
